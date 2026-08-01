c093e8cd7453ac0afb59894464d94de06fce8de3
ICHINOSE Shogo <shogo82148@gmail.com>
2026-08-01 09:53:38 +0900

    test: add unit test for CloseStatus function

---
+++ conn_test.go
@@ -0,0 +1,27 @@
+package websocket
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestCloseStatus(t *testing.T) {
+	tests := []struct {
+		err      error
+		expected StatusCode
+	}{
+		{nil, -1},
+		{errors.New("some error"), -1},
+		{CloseError{Code: StatusNormalClosure, Reason: "normal closure"}, StatusNormalClosure},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.err), func(t *testing.T) {
+			got := CloseStatus(test.err)
+			if got != test.expected {
+				t.Errorf("CloseStatus(%v) = %d; want %d", test.err, got, test.expected)
+			}
+		})
+	}
+}
