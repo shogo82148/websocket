7aceae12b0f99a2d8e1c513691d737b42dd73323
ICHINOSE Shogo <shogo82148@gmail.com>
2026-08-01 09:24:28 +0900

    feat: implement WebSocket connection methods and message handling stubs

--- conn.go
+++ conn.go
@@ -1,8 +1,21 @@
 package websocket
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
+)
+
+// MessageType represents the type of a WebSocket message.
+type MessageType int
+
+const (
+	// MessageText is for UTF-8 encoded text messages like JSON.
+	MessageText MessageType = 1
+
+	// MessageBinary is for binary messages like protobufs.
+	MessageBinary MessageType = 2
 )
 
 type Conn struct{}
@@ -51,6 +64,64 @@ func CloseStatus(err error) StatusCode {
 	return -1
 }
 
+// Ping sends a ping to the peer and waits for a pong.
+func (c *Conn) Ping(ctx context.Context) error {
+	return errors.New("not implemented")
+}
+
+// Reader reads from the connection until there is a WebSocket data message to be read.
+// It will handle ping, pong and close frames as appropriate.
+func (c *Conn) Reader(ctx context.Context) (MessageType, io.Reader, error) {
+	return 0, nil, errors.New("not implemented")
+}
+
+// Read reads a single WebSocket message from the connection.
+// It will handle ping, pong and close frames as appropriate.
+func (c *Conn) Read(ctx context.Context) (MessageType, []byte, error) {
+	return 0, nil, errors.New("not implemented")
+}
+
+// Writer returns a writer bounded by the context that will write a WebSocket message of type dataType to the connection.
+//
+// You must close the writer once you have written the entire message.
+//
+// Only one writer can be open at a time, multiple calls will block until the previous writer is closed.
+func (c *Conn) Writer(ctx context.Context, dataType MessageType) (io.WriteCloser, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Write writes a message to the connection.
+func (c *Conn) Write(ctx context.Context, dataType MessageType, data []byte) error {
+	return errors.New("not implemented")
+}
+
+// SetReadLimit sets the max number of bytes to read for a single message.
+// It applies to the Reader and Read methods.
+//
+// By default, the connection has a message read limit of 32768 bytes.
+//
+// When the limit is hit, reads return an error wrapping ErrMessageTooBig and the connection is closed with StatusMessageTooBig.
+//
+// Set to -1 to disable.
+func (c *Conn) SetReadLimit(limit int64) {
+	// TODO: implement SetReadLimit
+}
+
+// Subprotocol returns the negotiated subprotocol.
+// An empty string means the default protocol.
+func (c *Conn) Subprotocol() string {
+	return ""
+}
+
 func (c *Conn) Close(code StatusCode, reason string) error {
 	return errors.New("not implemented")
+}
+
+func (c *Conn) CloseNow() error {
+	return errors.New("not implemented")
+}
+
+func (c *Conn) CloseRead(ctx context.Context) context.Context {
+	// TODO: implement CloseRead
+	return ctx
 }
