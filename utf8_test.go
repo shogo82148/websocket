4f57a782e44dfdbd345d2019a9e6c7a6a0d37d4b
ICHINOSE Shogo <shogo82148@gmail.com>
2026-08-16 16:20:13 +0900

    test: add fuzzing and benchmarking for UTF-8 reader

--- utf8_test.go
+++ utf8_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"strings"
 	"testing"
+	"unicode/utf8"
 )
 
 var utf8TestCases = []struct {
@@ -71,5 +72,51 @@ func TestUTF8Writer(t *testing.T) {
 				t.Errorf("expected %q, got %q", tc.input, rwc.w.String())
 			}
 		})
+	}
+}
+
+func FuzzUTF8Reader(f *testing.F) {
+	for _, tc := range utf8TestCases {
+		f.Add(tc.input)
+	}
+	f.Fuzz(func(t *testing.T, input string) {
+		valid := utf8.ValidString(input)
+		ctx := t.Context()
+		conn, _ := newTestConnWithInput(t, []byte{})
+		r := strings.NewReader(input)
+		reader := &utf8Reader{ctx: ctx, r: r, conn: conn}
+		_, err := io.Copy(io.Discard, reader)
+		if (err == nil) != valid {
+			t.Errorf("expected valid=%v, got err=%v", valid, err)
+		}
+	})
+}
+
+func BenchmarkUTF8Reader(b *testing.B) {
+	var r strings.Reader
+	input := strings.Repeat("Hello, 世界🍺", 1000)
+	ctx := b.Context()
+	conn, _ := newTestConnWithInput(b, []byte{})
+	reader := &utf8Reader{conn: conn}
+	b.ResetTimer()
+	b.SetBytes(int64(len(input)))
+	for b.Loop() {
+		r.Reset(input)
+		reader.reset(ctx, &r)
+		_, err := io.Copy(io.Discard, reader)
+		if err != nil {
+			b.Fatalf("io.Copy failed: %v", err)
+		}
+	}
+}
+
+func BenchmarkUTF8(b *testing.B) {
+	input := strings.Repeat("Hello, 世界🍺", 1000)
+	b.ResetTimer()
+	b.SetBytes(int64(len(input)))
+	for b.Loop() {
+		if !utf8.ValidString(input) {
+			b.Fatalf("invalid UTF-8 string")
+		}
 	}
 }
